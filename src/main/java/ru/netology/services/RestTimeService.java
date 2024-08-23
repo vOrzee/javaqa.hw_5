c512ba513eceea6a88ca62a0fdb04c694ebfd49a
Roman Leshin <vorzee@gmail.com>
2024-08-23 15:08:13 +0500

    Реализован метод calculate класса RestTimeService согласно ТЗ

--- src/main/java/ru/netology/services/RestTimeService.java
+++ src/main/java/ru/netology/services/RestTimeService.java
@@ -1,15 +1,15 @@
 package ru.netology.services;
 
 public class RestTimeService {
-    public int calculate(int income, int expenses) {
+    public int calculate(int income, int expenses, int threshold) {
         int count = 0; // счётчик месяцев отдыха
         int money = 0; // количество денег на счету
         for (int month = 0; month < 12; month++) {
-            if (money >= expenses) { // можем ли отдыхать?
+            if (money >= threshold) { // можем ли отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
-                money = money - expenses;
+                money = (money - expenses) / 3;
             } else {
-                money = money + income;
+                money = money + income - expenses;
             }
         }
         return count;
